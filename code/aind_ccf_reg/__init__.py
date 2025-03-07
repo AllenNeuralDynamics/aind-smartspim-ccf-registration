5e14d2bc3b3f2682d9ceb1925c28531fa6c53e6c
nal10 <nal10@users.noreply.github.com>
2025-03-07 15:55:54 -0800

    Update __init__.py

--- code/aind_ccf_reg/__init__.py
+++ code/aind_ccf_reg/__init__.py
@@ -1,4 +1,40 @@
 """CCF Registration package.
 """
 
-__version__ = "0.0.28"
+__version__ = "0.0.29"
+
+__authors__ = [
+    "Camilo Laiton",
+    "Di Wang",
+    "Nicholas Lusk",
+    "Yoni Browning",
+    "Galen Lynch",
+]
+__author_emails__ = [
+    "camilo.laiton@alleninstitute.org",
+    "di.wang@alleninstitute.org",
+    "nicholas.lusk@alleninstitute.org",
+    "yoni.browning@alleninstitute.org",
+    "galen.lynch@alleninstitute.org",
+]
+__license__ = "MIT"
+__description__ = (
+    "Repository for aligning brains captures with "
+    "the SmartSPIM lightsheet microscope to the Allen CCF V3."
+)
+__url__ = "https://github.com/AllenNeuralDynamics/aind-smartspim-destripe"
+
+__maintainers__ = [
+    "Camilo Laiton",
+    "Di Wang",
+    "Nicholas Lusk",
+    "Yoni Browning",
+]
+__maintainer_emails__ = [
+    "camilo.laiton@alleninstitute.org",
+    "di.wang@alleninstitute.org",
+    "nicholas.lusk@alleninstitute.org",
+]
+__title__ = "aind-smartspim-destripe"
+__status__ = "Production"  # 'Development' 'Production', 'Beta'
+__pipeline_version__ = "3.0.0"
