db7de4efa63fe6ad2d2ccc0120c5874aaa6112b7
Camilo Laiton <36769694+camilolaiton@users.noreply.github.com>
2026-03-06 15:18:39 -0800

    updating metadata

--- code/aind_ccf_reg/__init__.py
+++ code/aind_ccf_reg/__init__.py
@@ -1,6 +1,6 @@
 """CCF Registration package."""
 
-__version__ = "0.0.35"
+__version__ = "0.0.36"
 
 __authors__ = [
     "Camilo Laiton",
@@ -21,7 +21,7 @@ __description__ = (
     "Repository for aligning brains captures with "
     "the SmartSPIM lightsheet microscope to the Allen CCF V3."
 )
-__url__ = "https://github.com/AllenNeuralDynamics/aind-smartspim-destripe"
+__url__ = "https://github.com/AllenNeuralDynamics/aind-smartspim-ccf-registration"
 
 __maintainers__ = [
     "Camilo Laiton",
@@ -34,6 +34,6 @@ __maintainer_emails__ = [
     "di.wang@alleninstitute.org",
     "nicholas.lusk@alleninstitute.org",
 ]
-__title__ = "aind-smartspim-destripe"
+__title__ = "aind-smartspim-ccf-registration"
 __status__ = "Production"  # 'Development' 'Production', 'Beta'
 __pipeline_version__ = "5.0.0"
